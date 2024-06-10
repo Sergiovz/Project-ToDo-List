fa74f67510049841edda4d091a359675a0aa251a
SergioVZ <sergiovaldez599@gmail.com>
2024-06-10 04:52:52 -0300

    Inicio del Proyecto/ToDo_List

---
+++ main.js
@@ -0,0 +1,31 @@
+const button = document.querySelector('.boton');
+button.addEventListener('click', () => {
+    let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
+
+    if (nuevaTareaTexto === "") {
+        alert("Please, Enter a task!");
+        return;
+    }
+
+    let contenedorDiv = document.createElement("div");
+    contenedorDiv.classList.add("contenedorDesordenado");
+
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = nuevaTareaTexto;
+    nuevaTarea.classList.add("lista");
+
+
+    let buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "DELETE";
+    buttonDelete.classList.add("buttonColor");
+    buttonDelete.onclick = () => {
+        contenedorDiv.remove();
+    };
+
+    let listaDesordenada = document.getElementById("taskList");
+    listaDesordenada.append(contenedorDiv);
+    contenedorDiv.appendChild(nuevaTarea);
+    contenedorDiv.appendChild(buttonDelete);
+
+    document.getElementById("nuevaTarea").value = "";
+});
